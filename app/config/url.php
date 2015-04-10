68a35c520b5ec22d7f39ed322c2a8536e54f4e8c
palhimanshu1991 <pal.himanshu1991@gmail.com>
2015-04-10 22:52:51 +0530

    Update url.php

--- app/config/url.php
+++ app/config/url.php
@@ -2,7 +2,7 @@
 
 return
         array(
-            'home' => 'http://localhost/live/',
+            'home' => 'http://www.berdict.com/',
             'web' => 'http://www.berdict.com/',
             'fb_id' => '292311107459306',
             'fb_secret' => '35a2ef058c6e26504aa1f8a30494a532'
