2ddeefb48e6c51b2bbcfb8c89b7298d8ac1a4094
palhimanshu1991 <pal.himanshu1991@gmail.com>
2015-04-11 01:33:01 +0530

    uploading database foler
    uploading database foler

---
+++ app/database/seeds/FavouritesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+class FavouritesTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// Uncomment the below to wipe the table clean before populating
+		// DB::table('favourites')->truncate();
+
+		$favourites = array(
+
+		);
+
+		// Uncomment the below to run the seeder
+		// DB::table('favourites')->insert($favourites);
+	}
+
+}
